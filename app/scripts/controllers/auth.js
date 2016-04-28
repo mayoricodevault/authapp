43eefc2a0828459d9074c83fe026a77f033cf5d7
Mike Mayori <mmayorivera@gmail.com>
2016-04-28 00:32:18 -0400

    more changes cutomizing login

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -5,7 +5,9 @@ app.controller('AuthCtrl', ['$scope', 'auth', 'toastr', function($scope, auth, t
   toastr.success('demo@mojix.io, pass: demo', 'Login default data!', {progressBar: true, timeOut: '15000'});
 
   $scope.auth = auth;
-  $scope.configs = { dict: {
+  $scope.configs = {
+    icon: 'http://www.mojix.com/wp-content/uploads/2015/10/logo-mojix.svg',
+    dict: {
     signin: {
       title: 'Login me in',
           emailPlaceholder: 'something@youremail.com'
